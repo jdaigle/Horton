032010aae5963e76a7d45372d9d863868e358b90
Joseph Daigle <joseph@cridion.com>
2011-02-03 21:14:27 -0500

    fixed a bug that required the subdirectories to exist

--- SqlPatch/FileLoader.cs
+++ SqlPatch/FileLoader.cs
@@ -3,40 +3,51 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace SqlPatch {
-    public class FileLoader {
+namespace SqlPatch
+{
+    public class FileLoader
+    {
 
         private readonly string path;
 
-        public FileLoader(string path) {
+        public FileLoader(string path)
+        {
             this.path = path;
             Files = new Dictionary<Guid, ScriptFile>();
         }
 
         public Dictionary<Guid, ScriptFile> Files { get; private set; }
 
-        public void LoadAllFiles() {
+        public void LoadAllFiles()
+        {
             LoadChangeScripts();
             LoadDatabaseObjects("views\\");
             LoadDatabaseObjects("sprocs\\");
         }
 
-        private void LoadChangeScripts() {
+        private void LoadChangeScripts()
+        {
             var scripts = new DirectoryInfo(path);
-            var files = scripts.GetFiles("*.sql", SearchOption.TopDirectoryOnly).OrderBy(x => {
+            var files = scripts.GetFiles("*.sql", SearchOption.TopDirectoryOnly).OrderBy(x =>
+            {
                 return Int32.Parse(x.Name.Substring(0, x.Name.IndexOf('_')));
             }).Select(x => new ScriptFile(x.FullName, x.Name, ScriptType.ChangeScript)).ToList();
-            foreach (var file in files) {
+            foreach (var file in files)
+            {
                 file.Load();
                 Files.Add(file.Id, file);
             }
         }
 
-        private void LoadDatabaseObjects(string subdirectory) {
+        private void LoadDatabaseObjects(string subdirectory)
+        {
+            if (!Directory.Exists(Path.Combine(path, subdirectory)))
+                return;
             var scripts = new DirectoryInfo(Path.Combine(path, subdirectory));
             var files = scripts.GetFiles("*.sql", SearchOption.TopDirectoryOnly)
                 .Select(x => new ScriptFile(x.FullName, x.Name, ScriptType.DatabaseObject)).ToList();
-            foreach (var file in files) {
+            foreach (var file in files)
+            {
                 file.Load();
                 Files.Add(file.Id, file);
             }
