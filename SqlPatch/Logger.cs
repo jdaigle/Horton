d4bc5a8a0ddac5e2c9db3eb0d2b4d717e35870b2
Joseph Daigle <joseph@cridion.com>
2011-02-03 21:14:13 -0500

    no more logging

--- SqlPatch/Logger.cs
+++ SqlPatch/Logger.cs
@@ -1,37 +1,42 @@
 ﻿using System;
-using System.IO;
 
-namespace SqlPatch {
-    public static class Logger {
+namespace SqlPatch
+{
+    public static class Logger
+    {
 
         private static string logname;
 
-        static Logger() {
+        static Logger()
+        {
             logname = "SqlPatch." + DateTime.UtcNow.Ticks.ToString() + ".log";
         }
 
         private static int indentLevel = 0;
 
-        public static void WriteLine(string value) {
-            for (int i = 0; i < indentLevel; i++) {
+        public static void WriteLine(string value)
+        {
+            for (int i = 0; i < indentLevel; i++)
+            {
                 Console.Write("\t");
-                File.AppendAllText(logname, "\t");
             }
             Console.WriteLine(value);
-            File.AppendAllText(logname, value + "\n");
         }
 
-        public static void Indent() {
+        public static void Indent()
+        {
             indentLevel++;
         }
 
-        public static void Unindent() {
+        public static void Unindent()
+        {
             indentLevel--;
             if (indentLevel < 0)
                 indentLevel = 0;
         }
 
-        public static void Reset() {
+        public static void Reset()
+        {
             indentLevel = 0;
         }
     }
