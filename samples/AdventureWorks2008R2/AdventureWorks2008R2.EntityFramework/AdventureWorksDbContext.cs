b87fa06cbbedd0bd03e59892484316486b76ab57
Joseph Daigle <joseph.daigle@gmail.com>
2016-09-08 15:39:14 -0400

    adding sample EF project to demo migration gen

---
+++ samples/AdventureWorks2008R2/AdventureWorks2008R2.EntityFramework/AdventureWorksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace AdventureWorks2008R2.EntityFramework
+{
+    public class AdventureWorksDbContext : DbContext
+    {
+        // this constuctor is required by Horton
+        public AdventureWorksDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Configurations.AddFromAssembly(typeof(AdventureWorksDbContext).Assembly);
+        }
+    }
+}
