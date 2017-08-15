b78de62bfaebfaded542900f74dbc51ea8d8c3c8
Joseph Daigle <joseph.daigle@gmail.com>
2017-08-15 09:24:11 -0400

    updating README and license info

--- src/Horton/Properties/AssemblyInfo.cs
+++ src/Horton/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Horton")]
 [assembly: AssemblyDescription("The simple database migration utility.")]
 [assembly: AssemblyProduct("Horton. The simple database migration utility.")]
-[assembly: AssemblyCopyright("Copyright (c) 2017 Joseph Daigle")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 Joseph Daigle and other contributors")]
 
 [assembly: ComVisible(false)]
 
